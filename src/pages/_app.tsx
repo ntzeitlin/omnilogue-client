0bd1c41752a3457c32589e0be8be6e97b1b78c36
Nate Z <ntzeitlin@gmail.com>
2025-04-25 10:17:16 -0500

    added radix-ui theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import "@/styles/globals.css";
+import '@radix-ui/themes/styles.css';
+import { Theme } from "@radix-ui/themes";
+
+
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+  <Theme appearance='dark'>
+    <Component {...pageProps} />
+  </Theme>
+)
 }
