b9c48490ec690ceaba226a1195a907d9f3787ab4
Nate Z <ntzeitlin@gmail.com>
2025-04-29 15:41:03 -0500

    users can now add stories to their bookshelf

--- src/components/storyoverview.tsx
+++ src/components/storyoverview.tsx
@@ -1,4 +1,7 @@
-import { Box, Card, Flex, Text } from "@radix-ui/themes"
+import { useAuthToken } from "@/auth/queries";
+import { getBookshelf } from "@/data/stories";
+import { Box, Button, Card, Flex, Text } from "@radix-ui/themes"
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import Link from "next/link";
 
 interface Author {
@@ -39,6 +42,51 @@ interface Story {
 export const StoryOverviewCard: React.FC<StoryOverviewCardProps> = ({story}) => {
     const {id, is_public, author, title, subtitle, category, story_tags, average_rating} = story
 
+    const {token} = useAuthToken()
+    const queryClient = useQueryClient()
+    const {data: bookshelf} = useQuery({
+      queryKey: ['bookshelf', token],
+      queryFn: async () => { return await getBookshelf(token)}
+    })
+
+    // Check to see if the current book is on the bookshelf
+    // Toggle between mutation functions depending on presence
+    // Toggle between button styles
+
+    const isOnBookshelf = () => {
+      return bookshelf?.some(story => story.story.id === id)
+    }
+    
+    const toggleBookshelfMutation = useMutation({
+      mutationFn: async () => {
+        if (!isOnBookshelf()) {
+          const response = await fetch("http://localhost:8000/bookshelves", {
+            method: 'POST',
+            headers: {
+              Authorization: `Token ${token}`,
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              "story_id": id
+            })
+          })
+          if (!response.ok) {
+            throw new Error('Failed to add to bookshelf')
+          }
+          const data = await response.json()
+          return data
+        }
+
+        // if already on bookshelf, run delete 
+
+        
+
+      },
+      onSettled: () => {
+        queryClient.invalidateQueries({queryKey: ['bookshelf', token]})
+      }
+    })
+
     if (!is_public){
         return ""
     }
@@ -47,7 +95,7 @@ export const StoryOverviewCard: React.FC<StoryOverviewCardProps> = ({story}) => 
     return (
         <Box>
             <Card>
-                <Flex gap="4" justify="center"> 
+                <Flex gap="4" justify="center" align="center"> 
                     <Flex direction="column" align="center">
                         <Link href={`/library/stories/${id}`}>
                           <Text>
@@ -70,6 +118,8 @@ export const StoryOverviewCard: React.FC<StoryOverviewCardProps> = ({story}) => 
                     <Flex align="center">
                        Average Rating: {average_rating || "No Reviews"}
                     </Flex>
+                    {isOnBookshelf() ? "Already there" : <Button onClick={()=> {toggleBookshelfMutation.mutate()}}>Add to Bookshelf</Button> }
+                    
                 </Flex>
             </Card>
         </Box>
