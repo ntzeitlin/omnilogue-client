ff085f10ad2c195b7d590e0d5e8faa18af1bd3c4
Nate Z <ntzeitlin@gmail.com>
2025-04-30 15:24:46 -0500

    New Story form v1 functioning

--- src/pages/office/stories/new/index.tsx
+++ src/pages/office/stories/new/index.tsx
@@ -1,13 +1,13 @@
 import { useAuthToken } from "@/auth/queries";
 import { NavBar } from "@/components/navbar"
 import { getCategories } from "@/data/categories";
-import { Button, Card, Flex, Text, TextArea, TextField } from "@radix-ui/themes"
-import { useQuery } from "@tanstack/react-query";
+import { Button, Card, Flex, Select, Text, TextArea, TextField } from "@radix-ui/themes"
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { useState } from "react";
-
 
 export default function NewStory() {
     const {token} = useAuthToken()
+    const queryClient = useQueryClient()
 
     const [story, setStory] = useState({
         title: "",
@@ -15,18 +15,42 @@ export default function NewStory() {
         description: "",
         excerpt: "",
         is_public: false,
-        category: null
+        category: "",
+        content: ""
     })
 
     const {data: categories, isLoading} = useQuery({
         queryKey: ['categories'],
         queryFn: async () => {
             return await getCategories(token)
+        },
+        enabled: !!token
+    })
+
+    const storySubmissionMutation = useMutation({
+        mutationFn: async () => {
+            const response = await fetch("http://localhost:8000/stories", {
+                method: "POST",
+                headers: {
+                    Authorization: `Token ${token}`,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(story)
+            })
+            if (!response.ok) {
+                throw new Error('Failed to create story')
+            }
+            const data = await response.json()
+            return data
+        },
+        onSettled: () => {
+            queryClient.invalidateQueries({queryKey: ['stories_overview']})
         }
     })
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        storySubmissionMutation.mutate()
     }
 
     return (
@@ -36,7 +60,27 @@ export default function NewStory() {
         <form onSubmit={handleSubmit}>
             <Flex direction="column" gap="3">
                 <Flex direction="column" gap="1">
-                    <Text as="label" htmlFor="storytitle" size="2" weight="medium">Title</Text>
+                        <Text as="label" htmlFor="category" size="2" weight="medium">Category</Text>
+                        <Select.Root
+                            value={story.category || ""}
+                            onValueChange={(event) => {
+                                const copyStory = {...story}
+                                copyStory.category = event
+                                setStory(copyStory)
+                            }}
+                        >
+                            <Select.Trigger placeholder="Select Category" />
+                            <Select.Content>
+                                {categories?.map((category) => {
+                                    return (
+                                        <Select.Item key={category.id} value={category.name}>{category.name}</Select.Item>
+                                    )
+                                })}
+                            </Select.Content>
+                        </Select.Root>
+                    </Flex>
+                <Flex direction="column" gap="1">
+                    <Text as="label" htmlFor="storytitle" size="2" weight="medium">Story Information:</Text>
                     <TextField.Root
                         placeholder="Story Title"
                         id="storytitle"
@@ -50,7 +94,7 @@ export default function NewStory() {
                         required
                     />
                 </Flex>
-
+                
                 <TextField.Root
                     placeholder="Story subtitle"
                     type="text"
@@ -69,13 +113,43 @@ export default function NewStory() {
                         copyStory.description = event.target.value
                         setStory(copyStory)
                     }}
+                    required
                 />
+                <TextArea
+                    placeholder="Story Excerpt"
+                    value={story.excerpt}
+                    onChange={(event) => {
+                        const copyStory = {...story}
+                        copyStory.excerpt = event.target.value
+                        setStory(copyStory)
+                    }}
+                />
+
 
                 {/* version 1 story handling:
                     - TextArea for Sections / Chapters
+
+                    Version 2:
                     - Button to add new Section -> generates new TextArea
+
+                    Version 3: 
+                    - Upload Zip file of markdown files.
                 */}
 
+                <Flex direction="column" gap="1">
+                    <Text as="label" htmlFor="storycontent" size="2" weight="medium">Story Content:</Text>
+                        <TextArea
+            placeholder="Story Content"
+            value={story.content}
+            style={{minHeight: '200px'}}
+            onChange={(event) => {
+                const copyStory = {...story}
+                copyStory.content = event.target.value
+                setStory(copyStory)
+            }}
+        />  
+                </Flex>
+                <Button type="submit">Submit Story</Button>
             </Flex>
         </form>
     </Flex>
