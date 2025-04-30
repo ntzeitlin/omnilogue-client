3faad86a074ffb09ddaffd33d3740dd15e0c95af
Nate Z <ntzeitlin@gmail.com>
2025-04-30 11:15:34 -0500

    Added code to disable HMR temporarily

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  // webpackDevMiddleware: config => {
+  //   // Disable HMR
+  //   config.watchOptions = {
+  //     ignored: ['**/*'],
+  //     poll: false,
+  //   }
+  //   return config
+  // }
 };
 
 export default nextConfig;
