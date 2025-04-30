ab3239dc6fc2af3a16900d254f5fe97cacdb9f21
Nate Z <ntzeitlin@gmail.com>
2025-04-30 12:00:15 -0500

    added styling and first three fields for a new story

--- src/pages/office/new-story/index.tsx
+++ src/pages/office/new-story/index.tsx
@@ -1,10 +1,14 @@
+import { useAuthToken } from "@/auth/queries";
 import { NavBar } from "@/components/navbar"
-import { Button, Flex, Text } from "@radix-ui/themes"
+import { getCategories } from "@/data/categories";
+import { Button, Card, Flex, Text, TextArea, TextField } from "@radix-ui/themes"
 import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 
 
 export default function NewStory() {
+    const {token} = useAuthToken()
+
     const [story, setStory] = useState({
         title: "",
         subtitle: "",
@@ -17,7 +21,7 @@ export default function NewStory() {
     const {data: categories, isLoading} = useQuery({
         queryKey: ['categories'],
         queryFn: async () => {
-            
+            return await getCategories(token)
         }
     })
 
@@ -26,12 +30,51 @@ export default function NewStory() {
     }
 
     return (
+    <Card size="2" m="2" style={{maxWidth: '500px', margin: '0 auto'}}>
     <Flex direction="column" gap="4" style={{margin: '0, auto', maxWidth: '400px'}}>
         <Text size="5" weight="bold">Create a Story</Text>
         <form onSubmit={handleSubmit}>
+            <Flex direction="column" gap="3">
+                <Flex direction="column" gap="1">
+                    <Text as="label" htmlFor="storytitle" size="2" weight="medium">Title</Text>
+                    <TextField.Root
+                        placeholder="Story Title"
+                        id="storytitle"
+                        type="text"
+                        value={story.title}
+                        onChange={(event) => {
+                            const copyStory = {...story}
+                            copyStory.title = event.target.value
+                            setStory(copyStory)
+                        }}
+                        required
+                    />
+                </Flex>
 
+                <TextField.Root
+                    placeholder="Story subtitle"
+                    type="text"
+                    value={story.subtitle}
+                    onChange={(event) => {
+                        const copyStory = {...story}
+                        copyStory.subtitle = event.target.value
+                        setStory(copyStory)
+                    }}
+                />
+                <TextArea
+                    placeholder="Description"
+                    value={story.description}
+                    onChange={(event) => {
+                        const copyStory = {...story}
+                        copyStory.description = event.target.value
+                        setStory(copyStory)
+                    }}
+                />
+
+            </Flex>
         </form>
     </Flex>
+    </Card>
 )
 }
 
